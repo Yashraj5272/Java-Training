ad3fe391ee1656d6a7a4634d89d554f728c6c63b
Yashraj Kad <yashrajkad96k@gmail.com>
2026-06-01 21:27:03 +0530

    Add files via upload

---
+++ Loopcode.java
@@ -0,0 +1,9 @@
+public class Loopcode {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("7 x " + i + " = " + (7 * i));
+        }
+
+    }
+}
