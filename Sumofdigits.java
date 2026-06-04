5e4a00fd05b9c4c825e930b82eb40862ec8d61ae
Yashraj Kad <yashrajkad96k@gmail.com>
2026-06-04 15:43:13 +0530

    Add files via upload

---
+++ Sumofdigits.java
@@ -0,0 +1,17 @@
+public class Sumofdigits {
+
+    static int sumDigits(int n) {
+       
+        if (n == 0) {
+            return 0;
+        }
+
+        return (n % 10) + sumDigits(n / 10);
+    }
+
+    public static void main(String[] args) {
+        int n = 155852;
+
+        System.out.println("Sum of digits = " + sumDigits(n));
+    }
+}
