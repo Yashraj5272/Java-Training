204754a952fec6b5763a2475be5293534f98fb63
Yashraj Kad <yashrajkad96k@gmail.com>
2026-06-04 14:05:39 +0530

    Add files via upload

---
+++ Recursionexample.java
@@ -0,0 +1,17 @@
+public class Recursionexample {
+    static int factorial(int n) {
+
+        if (n <=1) {
+            return 1;
+        } else {
+            return n* factorial(n-1);
+
+        }
+    }
+    public static void main(String[]args){
+        int num = 5;
+        System.out.println("Factorial = " + factorial(num));
+        
+    }
+        }
+    
